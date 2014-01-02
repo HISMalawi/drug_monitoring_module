29152063177bfe59791a3761fcddffaf5e657231
timmtonga <timmtonga@gmail.com>
2014-01-02 16:32:51 +0200

    homepage and layouts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrugMonitoringProgram::Application.config.secret_token = '65d9a66f47c63562978ff733aa13ba8657786abbb33d0fe6ad231b2ac3f54c64c2789acda0817ca95465b6bdc36bd0ead0e9ac8e8e190e64f1a8e5aad9117f8e'
