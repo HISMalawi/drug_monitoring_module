222b7c4a002f38021f41e3522766b8da2b6f749f
Kenneth <vine@vine.(none)>
2014-03-07 15:59:39 +0200

    added methods to calculate estimated stockout dates

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -4,4 +4,39 @@ class Observation < ActiveRecord::Base
   belongs_to :definition, :foreign_key => :definition_id
   validates_presence_of :site_id
   validates_presence_of :definition_id
+
+  def self.drug_stock_out_predictions
+    
+    rates = self.daily_drug_dispensation_rates
+
+    result = {}
+    self.find_by_sql(
+      "SELECT site_id, value_drug AS drug_name, value_date AS date, value_numeric AS stock_level FROM observations
+        WHERE definition_id = (SELECT definition_id FROM definitions WHERE name = 'Stock' LIMIT 1)
+        GROUP BY site_id, value_drug"
+    ).each do |obs|
+
+      if (rates[obs.site_id][obs.drug_name].blank? rescue true) || rates[obs.site_id][obs.drug_name].to_i == 0
+          result[obs.drug_name] = nil
+          next
+      end
+      
+      result[obs.drug_name] = (obs.date.to_date + 
+          (obs.stock_level.to_i/rates[obs.site_id][obs.drug_name].to_f).days) 
+    end
+  
+    return result
+  end
+
+  def self.daily_drug_dispensation_rates
+
+    return  self.find_by_sql(
+      "SELECT site_id, value_drug AS drug_name, ROUND(AVG(value_numeric)) AS rate FROM observations
+        WHERE definition_id = (SELECT definition_id FROM definitions WHERE name = 'Dispensation' LIMIT 1)
+        GROUP BY site_id, value_drug"
+    ).inject({}){|result, obs|
+      result[obs.site_id] = {} if result[obs.site_id].blank?; result[obs.site_id][obs.drug_name] = obs.rate; result
+    }    
+  end
+  
 end
