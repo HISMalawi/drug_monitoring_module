9745a620596fef40928b848ae8c8ad8a946075ad
Kenneth <vine@vine.(none)>
2014-04-03 10:18:20 +0200

    added route for delivery report

--- config/routes.rb
+++ config/routes.rb
@@ -65,6 +65,7 @@ DrugMonitoringProgram::Application.routes.draw do
   get "report/stock_out_estimates"
   get "report/months_of_stock"
   get "report/stock_movement"
+  get "report/delivery_report"
 
   ################### HOME ##############################
 
@@ -121,7 +122,7 @@ DrugMonitoringProgram::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => "home#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
