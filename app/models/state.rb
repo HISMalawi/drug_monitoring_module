90eecbef3728404ee6d8816a98c039d93c3fc5f9
timothy <timothy@nkunguza.(none)>
2014-07-03 13:52:04 +0200

    added properties to state model

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,7 @@
 class State < ActiveRecord::Base
   # attr_accessible :title, :body
+  set_primary_key :state_id
+  belongs_to :observation, :foreign_key => :observation_id
+  validates_presence_of :observation_id
+
 end
