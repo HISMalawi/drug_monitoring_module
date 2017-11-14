fb47965a8181739ec94893e0aad9d0fbfe63fb67
mangochiman <mangochiman@gmail.com>
2017-11-14 10:56:09 +0200

    Updating data from couch to mysql every hour using cron job

--- script/update_details.rb
+++ script/update_details.rb
@@ -1,57 +1,79 @@
+def get_couch_changes
+  couch_mysql_path = Rails.root.to_s + "/config/couch_mysql.yml"
+  db_settings = YAML.load_file(couch_mysql_path)
+  couch_db_settings = db_settings["couchdb"]
+  couch_host = couch_db_settings["host"]
+  couch_db = couch_db_settings["database"]
+  couch_port = couch_db_settings["port"]
 
-require 'rest-client'
+  couch_address = "http://#{couch_host}:#{couch_port}/#{couch_db}/_changes?descending=true&include_docs=true"
+  received_params = RestClient.get(couch_address)
+  results = JSON.parse(received_params)
+  couch_data = {}
 
-$prescription_id = Definition.where(:name => "prescription").first.id
-$dispensation_id = Definition.where(:name => "dispensation").first.id
-$relocation_id = Definition.where(:name => "relocation").first.id
-$drug_given_to_id = Definition.where(:name => "People who received drugs").first.id
-$drug_prescribed_id = Definition.where(:name => "People prescribed drug").first.id
-$drug_stock_level_id = Definition.where(:name => "Stock level").first.id
-$drug_rate_id = Definition.where(:name => "Drug rate").first.id
-$receipts_id = Definition.where(:name => "New Delivery").first.id
-$clinic_id = Definition.where(:name => "Clinic verification").first.id
-$supervision_id = Definition.where(:name => "Supervision verification").first.id
-$supervision_in_detail_id = Definition.where(:name => "Supervision verification in detail").first.id
+  results.each do |key, values|
+    values.each do |data|
+      date = data["doc"]["date"].to_date.strftime('%Y-%m-%d') rescue nil
+      next if date.blank?
+      consumption_rate = data["doc"]["consumption_rate"]
 
-def start
+      dispensations = data["doc"]["dispensations"]
+      prescriptions = data["doc"]["prescriptions"]
+      receipts = data["doc"]["receipts"]
+      site_code = data["doc"]["site_code"]
+      stock_level = data["doc"]["stock_level"]
+      supervision_verification = data["doc"]["supervision_verification"]
+      supervision_verification_in_details = data["doc"]["supervision_verification_in_details"]
+      relocations = data["doc"]["relocations"]
 
-  sites = Site.where(:active => 1)
-  (sites || []).each do |site|
-    puts "Getting Data For Site #{site.name}"
-      date = PullTracker.where(:'site_id' => site.id).select('MAX(pulled_datetime) as pulled_datetime').first.pulled_datetime.to_date rescue Date.today
-      while date <= Date.today
+      couch_data["date"] = date
+      couch_data["site_code"] = site_code
+      couch_data["prescriptions"] = prescriptions
+      couch_data["dispensations"] = dispensations
+      couch_data["consumption_rate"] = consumption_rate
+      couch_data["receipts"] = receipts
+      couch_data["stock_level"] = stock_level
+      couch_data["supervision_verification"] = supervision_verification
+      couch_data["supervision_verification_in_details"] = supervision_verification_in_details
+      couch_data["relocations"] = relocations
 
-        url = "http://#{site.ip_address}:#{site.port}/drug/art_stock_info?date=#{date.to_date.to_s}"
-        begin
-          data = JSON.parse(RestClient::Request.execute(:method => :post, :url => url)) 
-          puts "............. #{site.name}: #{date}"
-          record(site,date ,data)
-          record_pulled_datetime(site, date)
-          date += 1.day
-        rescue 
-          puts "Failed to connect #{site.name}: #{date}"
-          break
-        end
-      end
+      create_or_update_mysql_from_couch(couch_data)
+    end rescue nil
   end
-
-  puts "Calculating Stock Levels"
-  `rails runner #{Rails.root}/script/calculate_stock_levels.rb`
-
+  return couch_data
+  #render :text => couch_data.to_json and return
 end
 
-def record(site, date,data)
+def create_or_update_mysql_from_couch(data)
+  prescription_id = Definition.where(:name => "prescription").first.id
+  dispensation_id = Definition.where(:name => "dispensation").first.id
+
+  relocation_id = Definition.where(:name => "relocation").first.id
+  drug_given_to_id = Definition.where(:name => "People who received drugs").first.id
+  drug_prescribed_id = Definition.where(:name => "People prescribed drug").first.id
+  drug_stock_level_id = Definition.where(:name => "Stock level").first.id
+  drug_rate_id = Definition.where(:name => "Drug rate").first.id
+  receipts_id = Definition.where(:name => "New Delivery").first.id
+  clinic_id = Definition.where(:name => "Clinic verification").first.id
+  supervision_id = Definition.where(:name => "Supervision verification").first.id
+  supervision_in_detail_id = Definition.where(:name => "Supervision verification in detail").first.id
+  month_of_stock_defn = Definition.find_by_name('Month of Stock').id
+  site_code = data["site_code"]
+  date = data["date"]
+  site_id = Site.find_by_site_code(site_code).id  rescue nil
 
   (data['prescriptions'] || []).each do |prescription|
-    pres_obs = Observation.where(:site_id => site.id,
-      :definition_id => $prescription_id,
+    pres_obs = Observation.where(:site_id => site_id,
+      :definition_id => prescription_id,
       :value_drug => prescription['drug_inventory_id'],
       :value_date => date
     ).first
 
+
     if pres_obs.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $prescription_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => prescription_id,
           :value_numeric => prescription['total'],
           :value_drug => prescription['drug_inventory_id'],
           :value_date => date})
@@ -60,15 +82,16 @@ def record(site, date,data)
       pres_obs.save
     end
 
-    pres_to = Observation.where(:site_id => site.id,
-      :definition_id => $drug_prescribed_id,
+    pres_to = Observation.where(:site_id => site_id,
+      :definition_id => drug_prescribed_id,
       :value_drug => prescription['drug_inventory_id'],
       :value_date => date
     ).first
 
     if pres_to.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $drug_prescribed_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => drug_prescribed_id,
           :value_numeric => prescription['total_patients'],
           :value_drug => prescription['drug_inventory_id'],
           :value_date => date})
@@ -79,15 +102,16 @@ def record(site, date,data)
   end
 
   (data['dispensations'] || []).each do |dispensation|
-    disp_obs = Observation.where(:site_id => site.id,
-      :definition_id => $dispensation_id,
+    disp_obs = Observation.where(:site_id => site_id,
+      :definition_id => dispensation_id,
       :value_drug => dispensation['drug_inventory_id'],
       :value_date => date
     ).first
 
     if disp_obs.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $dispensation_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => dispensation_id,
           :value_numeric => dispensation['total'],
           :value_drug => dispensation['drug_inventory_id'],
           :value_date => date})
@@ -96,15 +120,16 @@ def record(site, date,data)
       disp_obs.save
     end
 
-    disp_to = Observation.where(:site_id => site.id,
-      :definition_id => $drug_given_to_id,
+    disp_to = Observation.where(:site_id => site_id,
+      :definition_id => drug_given_to_id,
       :value_drug => dispensation['drug_inventory_id'],
       :value_date => date
     ).first
 
     if disp_to.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $drug_given_to_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => drug_given_to_id,
           :value_numeric => dispensation['total_patients'],
           :value_drug => dispensation['drug_inventory_id'],
           :value_date => date})
@@ -116,15 +141,16 @@ def record(site, date,data)
 
   (data['relocations'] || []).each do |key,value|
     next if value == 0
-    relocation_obs = Observation.where(:site_id => site.id,
-      :definition_id => $relocation_id,
+    relocation_obs = Observation.where(:site_id => site_id,
+      :definition_id => relocation_id,
       :value_drug => key,
       :value_date => date
     ).first
 
     if relocation_obs.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $relocation_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => relocation_id,
           :value_numeric => value,
           :value_drug => key,
           :value_date => date})
@@ -137,18 +163,19 @@ def record(site, date,data)
 
   (data['receipts'] || []).each do |key,value|
     next if value == 0
-    receipts_ob = Observation.where(:site_id => site.id,
-                                       :definition_id => $receipts_id,
-                                       :value_drug => key,
-                                       :value_date => date
+    receipts_ob = Observation.where(:site_id => site_id,
+      :definition_id => receipts_id,
+      :value_drug => key,
+      :value_date => date
     ).first
 
     if receipts_ob.blank?
-      Observation.create({:site_id => site.id,
-                          :definition_id => $receipts_id,
-                          :value_numeric => value,
-                          :value_drug => key,
-                          :value_date => date})
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => receipts_id,
+          :value_numeric => value,
+          :value_drug => key,
+          :value_date => date})
     else
       receipts_ob.value_numeric = value
       receipts_ob.save
@@ -157,15 +184,16 @@ def record(site, date,data)
   end
 
   (data['stock_level'] || []).each do |drug_id, value|
-    relocation_obs = Observation.where(:site_id => site.id,
-      :definition_id => $drug_stock_level_id,
+    relocation_obs = Observation.where(:site_id => site_id,
+      :definition_id => drug_stock_level_id,
       :value_drug => drug_id,
       :value_date => date
     ).first
 
     if relocation_obs.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $drug_stock_level_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => drug_stock_level_id,
           :value_numeric => value,
           :value_drug => drug_id,
           :value_date => date})
@@ -177,15 +205,16 @@ def record(site, date,data)
   end
 
   (data['consumption_rate'] || []).each do |drug_id, value|
-    relocation_obs = Observation.where(:site_id => site.id,
-      :definition_id => $drug_rate_id,
+    relocation_obs = Observation.where(:site_id => site_id,
+      :definition_id => drug_rate_id,
       :value_drug => drug_id,
       :value_date => date
     ).first
 
     if relocation_obs.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $drug_rate_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => drug_rate_id,
           :value_numeric => value.round(2),
           :value_drug => drug_id,
           :value_date => date})
@@ -198,15 +227,16 @@ def record(site, date,data)
 
   #.............................................................................
   (data['supervision_verification'] || []).each do |drug_id, value|
-    relocation_obs = Observation.where(:site_id => site.id,
-      :definition_id => $supervision_id,
+    relocation_obs = Observation.where(:site_id => site_id,
+      :definition_id => supervision_id,
       :value_drug => drug_id,
       :value_date => date
     ).first
 
     if relocation_obs.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $drug_rate_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => drug_rate_id,
           :value_numeric => value,
           :value_drug => drug_id,
           :value_date => date})
@@ -218,15 +248,16 @@ def record(site, date,data)
   end
 
   (data['clinic_verification'] || []).each do |drug_id, value|
-    relocation_obs = Observation.where(:site_id => site.id,
-      :definition_id => $clinic_id,
+    relocation_obs = Observation.where(:site_id => site_id,
+      :definition_id => clinic_id,
       :value_drug => drug_id,
       :value_date => date
     ).first
 
     if relocation_obs.blank?
-      Observation.create({:site_id => site.id,
-          :definition_id => $drug_rate_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => drug_rate_id,
           :value_numeric => value,
           :value_drug => drug_id,
           :value_date => date})
@@ -239,8 +270,8 @@ def record(site, date,data)
 
   (data['supervision_verification_in_details'] || []).each do |drug_id, values|
     next if values.blank?
-    relocation_obs = Observation.where(:site_id => site.id,
-      :definition_id => $supervision_in_detail_id,
+    relocation_obs = Observation.where(:site_id => site_id,
+      :definition_id => supervision_in_detail_id,
       :value_drug => drug_id,
       :value_date => date
     ).first
@@ -249,8 +280,9 @@ def record(site, date,data)
       value_text_str = "{previous_verified_stock:#{values['previous_verified_stock']},"
       value_text_str += "earliest_expiry_date:#{values['earliest_expiry_date']},"
       value_text_str += "expiring_units:#{values['expiring_units']}}"
-      Observation.create({:site_id => site.id,
-          :definition_id => $supervision_in_detail_id,
+      Observation.create({
+          :site_id => site_id,
+          :definition_id => supervision_in_detail_id,
           :value_numeric => values['verified_stock'],
           :value_drug => drug_id,
           :value_text => value_text_str,
@@ -267,19 +299,37 @@ def record(site, date,data)
 
   end
   #.............................................................................
-end
+  ##### calculating month of stock start #################
+  drugs = Observation.find_by_sql("SELECT DISTINCT value_drug FROM observations where voided = 0")
+  sites = Site.where(:active => 1)
+  (sites || []).each do |site|
+    puts "calculating for site : #{site.name}"
+    (drugs || []).each do |drug|
+      month_of_stock = Observation.calculate_month_of_stock(drug.value_drug, site.id)
 
-def record_pulled_datetime(site, date)
+      unless (month_of_stock.is_a? String ||  month_of_stock.nan? || month_of_stock.to_s.downcase == "infinity")
+        puts "Month of stock : #{month_of_stock} for drug #{drug.value_drug} "
+        Observation.create({:site_id => site.id,
+            :definition_id => month_of_stock_defn,
+            :value_numeric => month_of_stock.to_f.round(3),
+            :value_drug => drug.value_drug,
+            :value_date => Date.today})
 
-  pulled_time = PullTracker.where(:'site_id' => site.id).first
+      end
+    end
+  end
+  ##### calculating month of stock end#################
+
+  pulled_time = PullTracker.where(:'site_id' => site_id).first
 
   if pulled_time.blank?
     pulled_time = PullTracker.new()
-    pulled_time.site_id = site.id
+    pulled_time.site_id = site_id
   end
-  pulled_time.pulled_datetime = ("#{date.to_date} #{Time.now().strftime('%H:%M:%S')}")
+  pulled_time.pulled_datetime = ("#{Date.today} #{Time.now().strftime('%H:%M:%S')}")
   pulled_time.save
-  puts "Recorded for :#{site.name}, Date: #{pulled_time.pulled_datetime}"
+  #################### END ####################################################
+
 end
 
-start
+get_couch_changes
